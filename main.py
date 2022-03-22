0fb22b0d5a51f785d1196f2f14a015d01de08956
Piotr Sienski <huwzpf@o2.pl>
2022-03-22 04:50:20 +0100

    final refactoring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-from SMO import *
 import pandas as pd
+from SMO import SMO
 
 
 def main():
@@ -10,16 +9,9 @@ def main():
     for i in range(len(y)):
         if y[i] == 0:
             y[i] = -1
-    s = SMO(x, y)
-    pr = 0
+    s = SMO(x, y, 0.1, 100)
     s.train(0.0000005, 50)
-    for i in range(len(y)):
-        if s.hypothesis(x[i]) < 0:
-            pr = -1
-        else:
-            pr = 1
-        print(pr, y[i])
-
+    s.plot()
 
 if __name__ == '__main__':
     main()
